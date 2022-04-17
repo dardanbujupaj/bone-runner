ae58428905941384ed6d81eb59dcb381c8769841
Dardan Bujupaj <d.bujupaj@bluewin.ch>
2022-04-17 04:30:36 +0200

    Fix BonePurger. Fix font min size.

--- scenes/skeleton/BonePurger.gd
+++ scenes/skeleton/BonePurger.gd
@@ -6,6 +6,6 @@ func _process(delta: float) -> void:
 	var bones = get_tree().get_nodes_in_group("bones")
 	var bone_count = bones.size()
 	
-	for i in range(floor((bone_count - Settings.max_bones) / 10)):
+	for i in range(ceil((bone_count - Settings.max_bones) / 10)):
 		bones[i].queue_free()
 	
